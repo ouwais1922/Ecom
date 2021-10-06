b7817708c2dbf7151f72c61ca6bfa66fb3f719f1
~ouwais <ouwaisszlaigi2@gmail.com>
2021-10-06 20:30:27 +0100

    first_step

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        $usertype = Auth::User()->usertype;
+        
+        if($usertype == '1')
+        {
+            return view ('admin.home');
+        }
+        else
+        {
+            return view('user.home');
+        }
+
+    }
+
+    public function index()
+    {
+        return view ('user.home');
+    }
+
+
+}
